26a1269517756013adc418a380b1e2d97cd75492
pedrorfernandes <pedrorfernandes@yahoo.com>
2013-03-24 19:53:54 +0000

    initial commit with the graphviewer API, changed from linux to apple ifdefs

---
+++ Grafos/connection.h
@@ -0,0 +1,38 @@
+#ifndef _CONNECTION_
+#define _CONNECTION_
+
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+
+#ifdef __APPLE__
+#include <arpa/inet.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <netdb.h>
+#else
+#include <winsock2.h>
+#endif
+
+#include <string>
+#include <iostream>
+
+using namespace std;
+
+class Connection {
+public:
+    Connection(int port);
+    
+    bool sendMsg(string msg);
+    string readLine();
+private:
+#ifdef linux
+    int sock;
+#elif __APPLE__
+    int sock;
+#else
+    SOCKET sock;
+#endif
+};
+
+#endif
